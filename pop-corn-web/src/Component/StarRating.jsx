88d4f5ce4786599320e832c14a36768ff9fa9355
umarabid123 <umarabid709@gmail.com>
2024-07-18 16:27:41 +0500

    deploy

--- pop-corn-web/src/Component/StarRating.jsx
+++ pop-corn-web/src/Component/StarRating.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-function StarRating({ maxRating =8 }) {
-
+function StarRating({ maxRating = 8 }) {
   const containerStyle = {
     display: "flex",
     alignItems: "center",
@@ -10,11 +9,11 @@ function StarRating({ maxRating =8 }) {
 
   const starContainerStyle = {
     display: "flex",
-    alignItems :"center",
+    alignItems: "center",
     gap: "4px",
   };
   const textStyle = {
-    lineHeight: "0",
+    lineHeight: "1",
     margin: "0",
   };
 
@@ -22,7 +21,9 @@ function StarRating({ maxRating =8 }) {
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span style={textStyle}>S{i + 1}</span>
+          <span style={textStyle}>
+            <Star key={i} />
+          </span>
         ))}
       </div>
       <p>10</p>
@@ -31,3 +32,49 @@ function StarRating({ maxRating =8 }) {
 }
 
 export default StarRating;
+
+const starStyle = {
+  width: "48px",
+  height: "48px",
+  display: "block",
+  cursor: "pointer",
+};
+
+function Star() {
+  return (
+    <span role="button" style={starStyle}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="yellow"
+        // stroke={color}
+      >
+        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+      </svg> 
+    </span>
+  );
+}
+
+// full
+// {/* <svg
+// xmlns="http://www.w3.org/2000/svg"
+// viewBox="0 0 20 20"
+// fill={color}
+// stroke={color}
+// >
+// <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+// </svg> */}
+
+// half  <svg
+// xmlns="http://www.w3.org/2000/svg"
+// fill="none"
+// viewBox="0 0 24 24"
+// stroke={color}
+// >
+// <path
+//   strokeLinecap="round"
+//   strokeLinejoin="round"
+//   strokeWidth="{2}"
+//   d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+// />
+// </svg>
