a7d7c6b9f0e005ceed5b2a7e25c078af46fe9e06
umarabid123 <umarabid709@gmail.com>
2024-07-22 23:14:59 +0500

    deploy

--- pop-corn-web/src/App.jsx
+++ pop-corn-web/src/App.jsx
@@ -121,7 +121,7 @@ function Box({ children }) {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <>
-      <div className="box">
+      <div className="ox">
         <button
           className="btn-toggle"
           onClick={() => setIsOpen((open) => !open)}
