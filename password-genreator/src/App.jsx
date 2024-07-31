0b20aa51b91b8642641abbdac80585e61b48604c
umarabid123 <umarabid709@gmail.com>
2024-07-31 16:10:37 +0500

    deploy

---
+++ password-genreator/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+
+function App() {
+const [length, setLength] = useState(8)
+const [numberAllowed, setNumebrAllowed] = useState(true)
+
+  return (
+    <>
+     <div className="text-center text-7xl">hello world
+     </div>
+    </>
+  )
+}
+
+export default App
