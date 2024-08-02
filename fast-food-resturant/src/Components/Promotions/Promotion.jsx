0e59bb50b3c2afb4444a3b4495a3e0adf139b4a3
umarabid123 <umarabid709@gmail.com>
2024-08-02 09:59:19 +0500

    deploy

--- fast-food-resturant/src/Components/Promotions/Promotion.jsx
+++ fast-food-resturant/src/Components/Promotions/Promotion.jsx
@@ -7,11 +7,33 @@ const Promotion = () => {
       <div className="promotion-card">
         <img src="/src/assets/images/f2.png" alt="Burger" />
         <div className="promotion-content">
-            <div className="promotion-title">Tasty Thursdays</div>
-        <div className="promoion-discount">20% off</div>
-        <button className="order-button"> Order Now <FaShoppingCart /></button>
-      </div>
+          <div className="promotion-title">Tasty Thursdays</div>
+          <div className="promoion-discount">20% off</div>
+          <button className="order-button">
+            Order Now <FaShoppingCart />
+          </button>
         </div>
+      </div>
+      <div className="promotion-card">
+        <img src="/src/assets/images/f2.png" alt="Burger" />
+        <div className="promotion-content">
+          <div className="promotion-title">Tasty Thursdays</div>
+          <div className="promoion-discount">20% off</div>
+          <button className="order-button">
+            Order Now <FaShoppingCart />
+          </button>
+        </div>
+      </div>
+      <div className="promotion-card">
+        <img src="/src/assets/images/f6.png" alt="Pizza" />
+        <div className="promotion-content">
+          <div className="promotion-title">Pizza Days</div>
+          <div className="promoion-discount">15% off</div>
+          <button className="order-button">
+            Order Now <FaShoppingCart />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
