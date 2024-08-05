464f3438822ac4304923fdb9308781a59794897d
umarabid123 <umarabid709@gmail.com>
2024-08-05 19:51:42 +0500

    deploy

--- fast-food-resturant/src/Components/Burger-2/Burger.jsx
+++ fast-food-resturant/src/Components/Burger-2/Burger.jsx
@@ -7,6 +7,16 @@ const Burger = () => {
       <div className="burger-content">
         <img src="/src/assets/images/about-img.png" alt="about-img" />
       </div>
+      <div className="burger-text">
+        <h2>We are Feane</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. At aut
+          temporibus officiis autem voluptas, ad ipsa ullam excepturi blanditiis
+          sunt exercitationem veniam est corporis nihil porro vero labore
+          voluptates error!
+        </p>
+        <button className="read-more-button">Read More</button>
+      </div>
     </div>
   );
 };
