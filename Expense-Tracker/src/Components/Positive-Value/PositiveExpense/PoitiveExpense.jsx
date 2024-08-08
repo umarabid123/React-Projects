88bf8fcf46b99f196050b2ffc640eec8dac21d84
umarabid123 <umarabid709@gmail.com>
2024-08-08 19:39:36 +0500

    deploy

--- Expense-Tracker/src/Components/Positive-Value/PositiveExpense/PoitiveExpense.jsx
+++ Expense-Tracker/src/Components/Positive-Value/PositiveExpense/PoitiveExpense.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function PoitiveExpense({ item, customProp, inputAmount }) {
+function PoitiveExpense({ item, customProp}) {
   return (
-    <div className={`bg-white w-full border-r-4 flex justify-between items-center p-2 mt-4  hover:text-white transition-all duration-700 cursor-pointer ${item.amount > 0? "border-[#b6960a] hover:bg-[#b6960a] " :"border-[#2da3ad]  hover:bg-[#2da3ad]"} ${customProp}`}>
+    <div className={`bg-white w-full border-r-4 flex justify-between items-center p-2 mt-4  hover:text-white transition-all duration-700 cursor-pointer ${item.amount > 0? "border-[#b6960a] hover:bg-[#b6960a] " :"border-[#2da3ad]  hover:bg-[#2da3ad]"}`}>
       <p className="text-lg">{item.description}</p>
       <p className="text-lg">${item.amount}</p>
     </div>
