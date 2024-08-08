636d8e97907cc624d5e419423185edb1036d7af9
umarabid123 <umarabid709@gmail.com>
2024-08-08 19:49:37 +0500

    deploy

--- Expense-Tracker/src/Components/Positive-Value/PositiveExpense/PoitiveExpense.jsx
+++ Expense-Tracker/src/Components/Positive-Value/PositiveExpense/PoitiveExpense.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-function PoitiveExpense({ item, customProp}) {
+function PoitiveExpense({ item }) {
   return (
-    <div className={`bg-white w-full border-r-4 flex justify-between items-center p-2 mt-4  hover:text-white transition-all duration-700 cursor-pointer ${item.amount > 0? "border-[#b6960a] hover:bg-[#b6960a] " :"border-[#2da3ad]  hover:bg-[#2da3ad]"}`}>
-      <p className="text-lg">{item.description}</p>
-      <p className="text-lg">${item.amount}</p>
-    </div>
+    <>
+      <div className="flex flex-row-reverse items-center group relative">
+        <div
+          className={`bg-white w-full border-r-4 flex justify-between items-center p-2 mt-4   hover:text-white transition-all duration-700 cursor-pointer ${
+            item.amount > 0
+              ? "border-[#b6960a] hover:bg-[#b6960a] "
+              : "border-[#2da3ad]  hover:bg-[#2da3ad]"
+          }`}
+        >
+          <p className="text-lg">{item.description}</p>
+          <p className="text-lg">${item.amount}</p>
+        </div>
+        <p className="bg-black p-2 text-white h-10 hidden group-hover:block absolute -left-12">cross</p>
+      </div>
+    </>
   );
 }
 
-export default PoitiveExpense;  
+export default PoitiveExpense;
