5455eac8d2dce33431e9dd23823052904f7df2ae
umarabid123 <umarabid709@gmail.com>
2024-08-11 21:33:04 +0500

    deploy

--- CRUD-APP/src/App.jsx
+++ CRUD-APP/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
+import Card from "./Components/Card/Card";
 import Header from "./Components/Header/Header_Input";
-
 
 function App() {
   return (
     <div className="app bg-[#f7f7f7] h-[100vh]">
       <h1 className="text-7xl text-sky-500 text-center pt-6 mb-10">CRUD APP</h1>
-
       <Header />
+      <Card />
     </div>
   );
 }
--- CRUD-APP/src/Components/Header/Header_Input.jsx
+++ CRUD-APP/src/Components/Header/Header_Input.jsx
@@ -5,34 +5,34 @@ function Header() {
     <div className="bg-white p-4">
       <div className="headerInput flex items-center justify-center gap-6">
         {" "}
-        <label className="text-xl font-medium font-sans">
+        <label className="text-xl font-medium font-sans text-sky-500">
           First Name: 
           <input
             type="text"
-            className="border ml-5 rounded-lg p-2 outline-sky-500 placeholder:text-gray-400"
+            className="border ml-5 rounded-lg p-2 text-black outline-sky-500 placeholder:text-gray-400"
             placeholder="Enter your last name"
           />
         </label>
-        <label className="text-xl font-medium font-sans">
+        <label className="text-xl font-medium font-sans text-sky-500">
           Last Name: 
           <input
             type="text"
-            className="border ml-5 rounded-lg p-2 outline-sky-500 placeholder:text-gray-400"
+            className="border ml-5 rounded-lg p-2 text-black outline-sky-500 placeholder:text-gray-400"
             placeholder="Enter your last name"
           />
         </label>
-        <label className="text-xl font-medium font-sans">
+        <label className="text-xl font-medium font-sans text-sky-500">
           Age: 
           <input
             type="text"
-            className="border ml-5 rounded-lg p-2 outline-sky-500 placeholder:text-gray-400"
+            className="border ml-5 rounded-lg p-2 text-black outline-sky-500 placeholder:text-gray-400"
             placeholder="Enter your age"
           />
         </label>
-        <button className="bg-sky-500 text-white p-2 w-24 rounded-lg hover:bg-white hover:border hover:text-sky-500 hover:border-sky-500 transition-all duration-500 text-xl">
+        <button className="bg-sky-500 text-white p-2 text-black w-24 rounded-lg hover:bg-white hover:border hover:text-sky-500 hover:border-sky-500 transition-all duration-500 text-xl">
           Save
         </button>
-        <button className="bg-red-500 text-white p-2 w-24 rounded-lg hover:bg-white hover:text-red-500 hover:border hover:border-red-500 transition-all duration-500 text-xl">
+        <button className="bg-red-500 text-white p-2 text-black w-24 rounded-lg hover:bg-white hover:text-red-500 hover:border hover:border-red-500 transition-all duration-500 text-xl">
           Clear
         </button>
       </div>
