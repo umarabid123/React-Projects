a38096e6c518987271cfab9e5c6ef953e9d5e1c0
umarabid123 <umarabid709@gmail.com>
2024-08-13 12:33:30 +0500

    deploy

---
+++ context-apis-project-2/src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./Components/Card";
+import ThemeBtn from "./Components/ThemeBtn";
+import { ThemeProvider } from "./Contexts/themes";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  const lightTheme =() =>{
+    setThemeMode("light")
+  }
+  
+  const darkTheme =() =>{
+    setThemeMode("dark")
+  }
+  return (
+    <>
+      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
