68455a93bc554ace900b937067eb3f8c4b036989
umarabid123 <umarabid709@gmail.com>
2024-08-20 12:05:52 +0500

    deploy

--- crud-operations/src/App.jsx
+++ crud-operations/src/App.jsx
@@ -5,22 +5,22 @@ import Input from "./Components/Inputs/Input";
 
 function App() {
   const [dataItem, setDataItem] = useState([]);
-  const [id, setId] = useState("")
-  const [name, setName] = useState("")
-  const [username, setUserName] = useState("")
-  const [email, setEmail] = useState("")
-  const [street, setStreet] = useState("")
-  const [suite, setsuite] = useState("")
-  const [city, setCity] = useState("")
-  const [zipCode, setZipCode] = useState("")
-  const [lat, setLat] = useState("")
-  const [lng, setLng] = useState("")
-  const [phoneNumber, setPhoneNumber] = useState("")
-  const [website, setWebsite] = useState("")
-  const [companyName, setCompanyName] = useState("")
-  const [companyCatch, setCompanyCatch] = useState("")
-  const [companyBs, setCompanyBs] = useState("")
- 
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [street, setStreet] = useState("");
+  const [suite, setsuite] = useState("");
+  const [city, setCity] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [lat, setLat] = useState("");
+  const [lng, setLng] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [website, setWebsite] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [companyCatch, setCompanyCatch] = useState("");
+  const [companyBs, setCompanyBs] = useState("");
+  const [indexNumber, setIndexNumber] = useState(null);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -36,20 +36,117 @@ function App() {
     }
   }
 
-  function handleEdit(){
-    set
+  function handleEdit(
+    i,
+    id,
+    name,
+    username,
+    email,
+    street,
+    suite,
+    city,
+    zipcode,
+    lat,
+    lng,
+    phone,
+    website,
+    companyName,
+    companyCatch,
+    companyBs
+  ) {
+    setId(id);
+    setName(name);
+    setUserName(username);
+    setEmail(email);
+    setStreet(street);
+    setsuite(suite);
+    setCity(city);
+    setZipCode(zipcode);
+    setLat(lat);
+    setLng(lng);
+    setPhoneNumber(phone);
+    setWebsite(website);
+    setCompanyName(companyName);
+    setCompanyCatch(companyCatch);
+    setCompanyBs(companyBs);
+    setIndexNumber(i);
   }
+  function handleEditValue() {
+    const temp = [...dataItem];
+    temp[indexNumber] ={id:id, name:name, username:username, email:email, street:street, suite:suite, city:city,zipCode:zipCode,lat:lat, lng:lng, phoneNumber:phoneNumber, website:website,companyName:companyName, companyCatch: companyCatch, companyBs:companyBs
+    }
+    setDataItem(temp);
+    setId("");
+    setName("");
+    setUserName("");
+    setEmail("");
+    setStreet("");
+    setsuite("");
+    setCity("");
+    setZipCode("");
+    setLat("");
+    setLng("");
+    setPhoneNumber("");
+    setWebsite("");
+    setCompanyName("");
+    setCompanyCatch("");
+    setCompanyBs("");
+    setIndexNumber(null)
+  }
+    
   return (
     <>
-     <div className="app">
-      <h1 className="text-sky-600 text-6xl text-center m-2">CRUD Operations</h1>
-      <Input />
-     <div className="flex justify-between flex-wrap p-4">
-        {dataItem.map((item, i) => (
-          <Card item={item} key={i} handleDelete={handleDelete} />
-        ))}
+      <div className="app">
+        <h1 className="text-sky-600 text-6xl text-center m-2">
+          CRUD Operations
+        </h1>
+        <Input
+          id={id}
+          name={name}
+          username={username}
+          email={email}
+          street={street}
+          suite={suite}
+          city={city}
+          zipCode={zipCode}
+          lat={lat}
+          lng={lng}
+          phoneNumber={phoneNumber}
+          website={website}
+          companyName={companyName}
+          companyCatch={companyCatch}
+          companyBs={companyBs}
+          handleEditValue ={handleEditValue}
+          indexNumber ={indexNumber}
+          setId={setId}
+          setName={setName}
+          setUserName={setUserName}
+          setEmail={setEmail}
+          setStreet={setStreet}
+          setsuite={setsuite}
+          setCity={setCity}
+          setZipCode={setZipCode}
+          setLat={setLat}
+          setLng={setLng}
+          setPhoneNumber={setPhoneNumber}
+          setWebsite={setWebsite}
+          setCompanyName={setCompanyName}
+          setCompanyCatch={setCompanyCatch}
+          setCompanyBs={setCompanyBs}
+
+        />
+        <div className="flex justify-between flex-wrap p-4">
+          {dataItem.map((item, i) => (
+            <Card
+              item={item}
+              key={i}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+              i={i}
+            />
+          ))}
+        </div>
       </div>
-     </div>
     </>
   );
 }
--- crud-operations/src/Components/Inputs/Input.jsx
+++ crud-operations/src/Components/Inputs/Input.jsx
@@ -1,27 +1,143 @@
-import React from 'react'
+import React from "react";
 
-const Input = () => {
+const Input = ({
+  id,
+  name,
+  username,
+  email,
+  street,
+  suite,
+  city,
+  zipCode,
+  lat,
+  lng,
+  phoneNumber,
+  website,
+  companyName,
+  companyCatch,
+  companyBs,
+  handleEditValue,
+  indexNumber,
+  setId,
+  setName,
+  setUserName,
+  setEmail,
+  setStreet,
+  setSuite,
+  setCity,
+  setZipCode,
+  setLat,
+  setLng,
+  setPhoneNumber,
+  setWebsite,
+  setCompanyName,
+  setCompanyCatch,
+  setCompanyBs,
+}) => {
   return (
     <div>
-      <form className='flex justify-between items-center flex-wrap m-4'>
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Id' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Name' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your UserName' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Email' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Street' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Suite' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your City' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Zip Code' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Lat' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Lng' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Phone' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Website' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Company Name' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Comapny catch Phrase' />
-      <input type="text" required className='w-[19%] p-2 border my-2' placeholder='Enter your Bs' />
-      </form>
+      <div className="flex justify-between items-center flex-wrap m-4">
+        <input
+          type="text"
+          value={id}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Id"
+          onChange={(e) => setId(e.target.value)}
+        />
+        <input
+          type="text"
+          value={name}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Name"
+        />
+        <input
+          type="text"
+          value={username}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your UserName"
+        />
+        <input
+          type="text"
+          value={email}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Email"
+        />
+        <input
+          type="text"
+          value={street}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Street"
+        />
+        <input
+          type="text"
+          value={suite}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Suite"
+        />
+        <input
+          type="text"
+          value={city}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your City"
+        />
+        <input
+          type="text"
+          value={zipCode}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Zip Code"
+          on
+        />
+        <input
+          type="text"
+          value={lat}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Lat"
+        />
+        <input
+          type="text"
+          value={lng}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Lng"
+        />
+        <input
+          type="text"
+          value={phoneNumber}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Phone"
+        />
+        <input
+          type="text"
+          value={website}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Website"
+        />
+        <input
+          type="text"
+          value={companyName}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Company Name"
+        />
+        <input
+          type="text"
+          value={companyCatch}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Comapny catch Phrase"
+        />
+        <input
+          type="text"
+          value={companyBs}
+          className="w-[19%] p-2 border my-2"
+          placeholder="Enter your Bs"
+        />
+        <button
+          className="bg-sky-700 text-white font-semibold text-xl py-2 px-4 rounded-lg mx-2"
+          onClick={handleEditValue}
+        >
+          {indexNumber !== null ? "Update" : "Add Card"}
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
