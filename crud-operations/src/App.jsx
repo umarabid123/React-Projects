bb62ac057b12a673aaff79205cefcb2d4882e032
umarabid123 <umarabid709@gmail.com>
2024-08-22 10:34:51 +0500

    deploy

--- crud-operations/src/App.jsx
+++ crud-operations/src/App.jsx
@@ -2,79 +2,80 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Components/Card/Card";
 import Input from "./Components/Inputs/Input";
+import PopUp from "./Components/PopUp/PopUp";
 
 function App() {
   const [dataItem, setDataItem] = useState([
     {
-        "id": 1,
-        "name": "Leanne Graham",
-        "username": "Bret",
-        "email": "Sincere@april.biz",
-        "address": {
-            "street": "Kulas Light",
-            "suite": "Apt. 556",
-            "city": "Gwenborough",
-            "zipcode": "92998-3874",
-            "geo": {
-                "lat": "-37.3159",
-                "lng": "81.1496"
-            }
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz",
+      address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+        geo: {
+          lat: "-37.3159",
+          lng: "81.1496",
         },
-        "phone": "1-770-736-8031 x56442",
-        "website": "hildegard.org",
-        "company": {
-            "name": "Romaguera-Crona",
-            "catchPhrase": "Multi-layered client-server neural-net",
-            "bs": "harness real-time e-markets"
-        }
+      },
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org",
+      company: {
+        name: "Romaguera-Crona",
+        catchPhrase: "Multi-layered client-server neural-net",
+        bs: "harness real-time e-markets",
+      },
     },
     {
-        "id": 2,
-        "name": "Ervin Howell",
-        "username": "Antonette",
-        "email": "Shanna@melissa.tv",
-        "address": {
-            "street": "Victor Plains",
-            "suite": "Suite 879",
-            "city": "Wisokyburgh",
-            "zipcode": "90566-7771",
-            "geo": {
-                "lat": "-43.9509",
-                "lng": "-34.4618"
-            }
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv",
+      address: {
+        street: "Victor Plains",
+        suite: "Suite 879",
+        city: "Wisokyburgh",
+        zipcode: "90566-7771",
+        geo: {
+          lat: "-43.9509",
+          lng: "-34.4618",
         },
-        "phone": "010-692-6593 x09125",
-        "website": "anastasia.net",
-        "company": {
-            "name": "Deckow-Crist",
-            "catchPhrase": "Proactive didactic contingency",
-            "bs": "synergize scalable supply-chains"
-        }
+      },
+      phone: "010-692-6593 x09125",
+      website: "anastasia.net",
+      company: {
+        name: "Deckow-Crist",
+        catchPhrase: "Proactive didactic contingency",
+        bs: "synergize scalable supply-chains",
+      },
     },
     {
-        "id": 3,
-        "name": "Clementine Bauch",
-        "username": "Samantha",
-        "email": "Nathan@yesenia.net",
-        "address": {
-            "street": "Douglas Extension",
-            "suite": "Suite 847",
-            "city": "McKenziehaven",
-            "zipcode": "59590-4157",
-            "geo": {
-                "lat": "-68.6102",
-                "lng": "-47.0653"
-            }
+      id: 3,
+      name: "Clementine Bauch",
+      username: "Samantha",
+      email: "Nathan@yesenia.net",
+      address: {
+        street: "Douglas Extension",
+        suite: "Suite 847",
+        city: "McKenziehaven",
+        zipcode: "59590-4157",
+        geo: {
+          lat: "-68.6102",
+          lng: "-47.0653",
         },
-        "phone": "1-463-123-4447",
-        "website": "ramiro.info",
-        "company": {
-            "name": "Romaguera-Jacobson",
-            "catchPhrase": "Face to face bifurcated interface",
-            "bs": "e-enable strategic applications"
-        }
-    }
-]);
+      },
+      phone: "1-463-123-4447",
+      website: "ramiro.info",
+      company: {
+        name: "Romaguera-Jacobson",
+        catchPhrase: "Face to face bifurcated interface",
+        bs: "e-enable strategic applications",
+      },
+    },
+  ]);
   const [id, setId] = useState("");
   const [name, setName] = useState("");
   const [username, setUserName] = useState("");
@@ -89,16 +90,16 @@ function App() {
   const [website, setWebsite] = useState("");
   const [companyName, setCompanyName] = useState("");
   const [catchPhrase, setCatchPhrase] = useState("");
-  const [bs,setBs] = useState("");
+  const [bs, setBs] = useState("");
   const [indexNumber, setIndexNumber] = useState(null);
 
   useEffect(() => {
     // fetch("https://jsonplaceholder.typicode.com/users")
-      // .then((res) => res.json())
-      // .then((data) => setDataItem(data));
-    }, []);
-    console.log("dataItem", dataItem.slice(0,3));
-    
+    // .then((res) => res.json())
+    // .then((data) => setDataItem(data));
+  }, []);
+  console.log("dataItem", dataItem.slice(0, 3));
+
   function handleDelete(id) {
     if (id > 0) {
       const temp = dataItem.filter((item, i) => item.id !== id);
@@ -107,7 +108,7 @@ function App() {
   }
   function handleAdd(e) {
     // const temp = [...dataItem, obj];
-    e.preventdefault()
+    e.preventdefault();
     setDataItem([
       ...dataItem,
       {
@@ -133,7 +134,7 @@ function App() {
     ]);
 
     // console.log('CompanyName: ', companyName);
-    
+
     setId("");
     setName("");
     setUserName("");
@@ -151,29 +152,25 @@ function App() {
     setBs("");
   }
 
-  function handleEdit(
-    i,
-    item
-  ) {
+  function handleEdit(i, item) {
     setId(item.id);
     setName(item.name);
     setUserName(item.username);
     setEmail(item.email);
-    setStreet(item.adress.street);
-    setsuite(item.suite);
-    setCity(item.adress.city);
+    setStreet(item.address.street);
+    setsuite(item.address.suite);
+    setCity(item.address.city);
     setZipCode(item.address.zipcode);
-    setLat(item.adress.geo.lat);
+    setLat(item.address.geo.lat);
     setLng(item.address.geo.lng);
     setPhone(item.phone);
     setWebsite(item.website);
     setCompanyName(item.company.name);
     setCatchPhrase(item.compnay.catchPhrase);
     setBs(item.company.bs);
-    
+
     setIndexNumber(i);
     // console.log("bs: ", Name, companyName);
-    
   }
   function handleEditValue() {
     const temp = [...dataItem];
@@ -258,102 +255,106 @@ function App() {
           setBs={setBs}
           handleAdd={handleAdd}
         />
-        <div className="flex justify-between flex-wrap p-4">
-        <table className="max-w-[100%] w-full mx-auto">
-            <thead className="flex justify-between">
-              <tr className="flex justify-between items-center w-full text-center max-w-[100%] hover:bg-gray-100">
-                <td className="text-xl h-12  w-[20%] text-center font-bold text-sky-500">
-                  Id
+        <div className=" p-4">
+          <table className="table-autow-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 w-full max-w-[100%]">
+            <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border">
+              <tr className="flex gap-2 items-center w-full text-center max-w-[100%] hover:bg-gray-100 pt-4 px-2">
+                <td className="text-lg h-12  w-[15%] font-bold text-sky-500">
+                  Id:Name
                 </td>
-                <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                   Name
+                <td className="text-lg h-12 w-[20%] font-bold text-sky-500">
+                  UserName:Email
                 </td>
-                <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                  User Name
+                <td className="text-lg h-12 w-[22%] font-bold text-sky-500">
+                  Street:Suite
                 </td>
-                <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                  Email
+                <td className="text-lg h-12 w-[23%] font-bold text-sky-500">
+                  City:Zip Code
                 </td>
-                <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                  Street
+                <td className="text-lg h-12 w-[18%] font-bold text-sky-500">
+                  Lat:Lng
                 </td>
-                <td className="text-xl h-12  w-[20%] text-center font-bold text-sky-500">
-                  Suite
-                  </td>
-                  <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                    City
-                  </td>
-                  <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                    Zip Code
-                  </td>
-                  <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                    Lat
-                  </td>
-                  <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                    Lng
-                  </td>
-                  <td className="text-xl h-12  w-[20%] text-center font-bold text-sky-500">
-                  Phone Number
-                  </td>
-                  <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                    Website
-                  </td>
-                  <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                    
-                  </td>
-                  <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                    Age
-                  </td>
-                  <td className="text-xl h-12 w-[20%] text-center font-bold text-sky-500">
-                    Action
-                  </td>
-                {/* <button className="bg-sky-500 m-2 text-white p-2 w-24 rounded-lg hover:bg-white hover:border hover:text-sky-500 hover:border-sky-500 transition-all duration-500 text-xl">
-              Save
-            </button>
-            <button className="bg-red-500 m-2 text-white p-2 w-24 rounded-lg hover:bg-white hover:text-red-500 hover:border hover:border-red-500 transition-all duration-500 text-xl">
-              Clear
-            </button> */}
+                <td className="text-lg h-12 font-bold text-sky-500 w-[25%]">
+                  Phone:Website
+                </td>
+                <td className="text-lg h-12 font-bold text-sky-500 w-[25%]">
+                  Company Name
+                </td>
+                <td className="text-lg h-12 font-bold text-sky-500 w-[20%]">
+                   Actions
+                    </td>
+                {/* <td className="text-lg h-12 font-bold text-sky-500">
+                  Lat:Lng
+                  </td> */}
               </tr>
             </thead>
-            {/* <tbody className="">
-              {data.map((item, i) => {
+            <tbody className="border">
+              {dataItem.map((item, i) => {
                 return (
                   <tr
                     key={i}
-                    className="flex justify-between items-center hover:text-sky-500 hover:bg-slate-100 "
+                    className="flex gap-2 justify-between items-center text-center border-b relative hover:text-sky-500 hover:bg-slate-100 p-2"
                   >
-                    <td className="text-xl h-12 w-[20%] text-center">
-                      {i + 1}
+                    <td className="text-[1rem] h-16 py-2 w-[18%]">
+                      {item.id}: <br />  {item.name}
                     </td>
-                    <td className="text-xl h-12 w-[20%] text-center">
-                      {item.firstName}
+                    <td className="text-[1rem] h-16 py-2 w-[20%]">
+                      {item.username}: <br /> {item.email}
                     </td>
-                    <td className="text-xl h-12 w-[20%] text-center">
-                      {item.lastName}
+                    <td className="text-[1rem] h-16 py-2 w-[22%]">
+                      {item.address.street}: <br />  {item.address.suite}
                     </td>
-                    <td className="text-xl h-12 w-[20%] text-center">
-                      {item.age}
+                    <td className="text-[1rem] h-16 py-2 w-[23%]">
+                      {item.address.city}: <br />  {item.address.zipcode}
                     </td>
-                    <div className="btn w-[20%] ">
+                    <td className="text-[1rem] h-16 py-2 w-[18%]">
+                      {item.address.geo.lat}: <br />  {item.address.geo.lng}
+                    </td>
+                    <td className="text-[1rem] h-16 py-2 w-[25%]">
+                      {item.phone}: <br />
+                      {item.website}
+                    </td>
+                    <td className="text-[1rem] h-16 py-2 w-[23%]">
+                      {item.company.name}: <br />
+        
+                    </td>
+                    <td className="text-4xl self-center cursor-pointer h-16 w-[20%]">
                       <button
-                        className="bg-sky-500 text-white p-2  w-24 rounded-lg transition-all duration-500 text-xl m-2 "
-                        onClick={() => handleEdit(item,i)}
+                        className="bg-sky-700 text-white font-semibold text-sm py-2 px-4 rounded-lg"
+                        onClick={() => handleEdit(i, item)}
                       >
                         Edit
                       </button>
+                      <button
+                        className="bg-red-500 text-white p-2 w-16 rounded-lg transition-all duration-500 text-sm m-2"
+                        onClick={() => hendleDelete(item.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+
+                    {/* <td className="text-lg h-16">
+                    </td> */}
+                    {/* <td className="text-lg px-2 h-12 w-[20%] text-center">
+                      {item.zipcode}
+                    </td> */}
+
+                    {/* <div className="btn w-[20%] ">
+                    <button className='bg-sky-700 text-white font-semibold text-xl py-2 px-4 rounded-lg' onClick={() => handleEdit(i,item)}>Edit</button>
                       <button
                         className="bg-red-500 text-white p-2 w-24 rounded-lg transition-all duration-500 text-xl m-2"
                         onClick={() => hendleDelete(item.id)}
                       >
                         Delete
-                      </button>
-                    </div>
+                      </button> */}
+                    {/* </div> */}
+                    <hr />
                   </tr>
                 );
               })}
-            </tbody> */}
+            </tbody>
           </table>
-          {dataItem.map((item, i) => (
+          {/* {dataItem.map((item, i) => (
             <Card
               item={item}
               key={i}
@@ -361,7 +362,7 @@ function App() {
               handleEdit={handleEdit}
               i={i}
             />
-          ))}
+          ))} */}
         </div>
       </div>
     </>
